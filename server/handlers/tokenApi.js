2d92c5654f4e629da525dd511684a26d01705f12
guss-alberto <96133902+guss-alberto@users.noreply.github.com>
2022-05-19 14:29:24 +0200

    token is now a parameter in /token api

--- server/handlers/tokenApi.js
+++ server/handlers/tokenApi.js
@@ -4,16 +4,8 @@ const router = express.Router();
 
 const jwt = require('jsonwebtoken');
 
-router.get('/:token', async (req, res) => {
-    // decode token, verifies secret and checks expiration
-    jwt.verify(req.params.token, process.env.TOKEN_SECRET, function ( err, decoded ) {
-        if (err) 
-            res.status(403).json({ success: false, message: 'Token not valid'});
-        else {
-            // if everything is good, save in req object for use in other routes
-            res.status(200).json({ success: false, tokenData: decoded});
-        }
-    });
+router.get('/', async (req, res) => {
+    res.status(200).json({ success: true, tokenData: req.loggedUser});
 })
 
 module.exports = router
