c779cb92bec78bc4f45a9eba8609c87d2a753b46
manuelcattelan <cattelanmanuel07@gmail.com>
2022-05-26 21:19:59 +0200

    clean up error handling for document handler

--- server/handlers/documentHandler.js
+++ server/handlers/documentHandler.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); // express library
+const path = require('path'); // path library
+const multer = require('multer'); // multer library to handle form-data
+const multerS3 = require('multer-s3'); // multerS3 library to handle document upload
+const AWS = require('aws-sdk'); // aws library to handle s3 bucket
 
-const multer = require('multer')
-const multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
-const path = require('path')
+// router object to create and manage requests 
+const router = express.Router();
 
 // import document model
 const Document = require('./../models/documentModel');
+// import user model
 const User = require('./../models/userModel');
 
-// initialize cloud storage
+// define amazon cloud storage
 const s3 = new AWS.S3({
     region: process.env.AWS_BUCKET_REGION,
     accessKeyId: process.env.AWS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_KEY
 }) 
 
-// initialize multer storage object
+// define multer storage object to handle document upload
 const upload = multer({
     storage: multerS3 ({
         s3: s3,
@@ -26,9 +28,11 @@ const upload = multer({
             cb(null, {fieldName: file.fieldname});
         },
         key: function (req, file, cb) {
+            // the document is saved with a name generated from the current date
             cb(null, Date.now().toString()+'.pdf')
         }
     }),
+    // check prerequisites for document upload
     fileFilter: (req, file, cb) => {
         // check for file extension before uploading
         if (file.mimetype === "application/pdf") {
@@ -45,11 +49,12 @@ const upload = multer({
     }
 });
 
-// upload file to s3 cloud storage
+// define function used to upload file to s3 storage using the upload middleware
 const uploadFile = upload.single('url');
 
 // route handler for document upload
 router.post('', async (request, result) => {
+    // upload document sent in form to s3 storage
     uploadFile(request, result, function(error) {
         // error returned by multer upload function
         if(error){
@@ -60,10 +65,11 @@ router.post('', async (request, result) => {
                         message: error.message
                     })
         }
-
+        // if here, the document was successfully uploaded to s3 storage
         // convert tag attribute from string with spaces to array of elements
-        tags = request.body.tag.split(" ");
-
+        let tags = request.body.tag.split(" ");
+        // retrieve document url from s3 bucket
+        let documentLocation = request.file.location;
         // create new document entry
         let document = new Document ({
             title: request.body.title,
@@ -72,21 +78,31 @@ router.post('', async (request, result) => {
             area: request.body.area,
             tag: tags,
             creationDate: new Date,
-            url: request.file.location,
+            url: documentLocation,
         })
-
         // store new document entry in database
-        document.save().then( document => {
-            console.log('-> document successfully uploaded to database')
-            result
-                .status(201)
-                .json({ 
-                    success: true,
-                    message: 'Document created successfully', 
-                    document
-                });
-            return;
-        })
+        document.save()
+            .then( document => {
+                // document was saved successfully
+                console.log('-> document successfully uploaded to database')
+                return result
+                    .status(201)
+                    .json({ 
+                        success: true,
+                        message: 'Document created successfully', 
+                        document
+                    });
+            })
+            .catch( error => {
+                // document save failed
+                console.log('-> document upload failed')
+                return result
+                    .status(400)
+                    .json({
+                        success: false,
+                        message: error.message
+                    })
+            })
     })
 });
 
@@ -94,34 +110,29 @@ router.post('', async (request, result) => {
 router.get('', async (request, result) => {
     let documents
     let documentStatus = request.query.status;
-
-    // retrieve all documents from database
+    // retrieve all documents from database if no status parameter was given
     if (!documentStatus){ documents = await Document.find({}); }
+    // check for validity of status parameter given 
     else if (!(documentStatus == "pending" || documentStatus == "public" || documentStatus == "reported")){
-        result
+        return result
             .status(400)
             .json({
                 success: false,
                 message: 'Unknown document status provided'
             })
-        return
     }
     else {
+        // if status == reported, return all public documents that are currently reported
         if (documentStatus == "reported") { documents = await Document.find({ status: "public", reported: true }) }
+        // if status == pending or status == public, return all public documents that are pending or public
         else {documents = await Document.find({ status: documentStatus })}
     }
-
-    // if no documents were found
+    // if no documents were found in the database
     if (!documents || documents.length == 0){
-        result
+        return result
             .status(204)
-            .json({
-                success: true,
-                message: 'No documents found',
-            })
-        return;
+            .send()
     }
-
     // if documents were found, return document list
     result
         .status(200)
@@ -132,42 +143,36 @@ router.get('', async (request, result) => {
         })
 });
 
-// route handler for listing a document by ID
+// route handler for retrieving a document by ID
 router.get('/:id', async (request, result) => {
     // check id length and id string format (must be hex)
     if(request.params.id.length != 24 || request.params.id.match(/(?![a-f0-9])\w+/)){
-        result
+        return result
             .status(400)
             .json({
                 success: false,
                 message: 'Invalid ID',
             })
-        return;
     }
-
     // find document by id parameter
     let document = await Document.findById(request.params.id);
-    
-    // if no document was found
+    // if no document was found in the database
     if (!document){
-        result
+        return result
             .status(404)
             .json({
                 success: true,
                 message: 'No document found with the given id',
             })
-        return;
     }
-
+    // retrieve user name of document author
     let author = await User.findById(document.author);
-
     if (!author)
         author = {username: '[deleted]'};
     else 
         author = {username: author.username, avatar: author.avatar};
-
     // if document was found return document
-    result
+    return result
         .status(200)
         .json({
             success: true,
@@ -179,34 +184,48 @@ router.get('/:id', async (request, result) => {
 
 // route handler for deleting a document by ID
 router.delete('/:id', async(request, result) => {
+    // check id length and id string format (must be hex)
+    if(request.params.id.length != 24 || request.params.id.match(/(?![a-f0-9])\w+/)){
+        return result
+            .status(400)
+            .json({
+                success: false,
+                message: 'Invalid ID',
+            })
+    }
     // look for document with provided id
     let document = await Document.findById(request.params.id).exec();
-
-    // if no document was found
+    // if no document was found in the database
     if (!document){
-        result
+        return result
             .status(404)
             .json({
                 success: true,
                 message: 'No document found with the given id',
             })
-        return;
     }
-
     // retrieve document name from url attribute
     let documentName = path.basename(document.url);
-
     // create params object for document deletion
     let params = {
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: documentName
     }
-
     // delete document from database
-    await document.deleteOne();
-
+    document.deleteOne()
+        .catch( error => {
+            // document deletion failed
+            console.log('-> document deletion failed')
+            return result
+                .status(400)
+                .json({
+                    success: false,
+                    message: error.message
+                })
+        })
     // delete document from cloud storage
     s3.deleteObject(params, function (error, data) {
+        // document deletion from cloud failed
         if (error){
             return result
                     .status(400)
@@ -215,6 +234,7 @@ router.delete('/:id', async(request, result) => {
                         message: error.message
                     })
         }
+        // document deletion from cloud succeeded
         return result
                 .status(200)
                 .send({
@@ -222,13 +242,21 @@ router.delete('/:id', async(request, result) => {
                     message: 'Document deleted'
                 })
     })
-} )
+})
 
 // route handler for updating the "reported" attribute on document report
 router.patch('/:id/report', async (request, result) => {
+    // check id length and id string format (must be hex)
+    if(request.params.id.length != 24 || request.params.id.match(/(?![a-f0-9])\w+/)){
+        return result
+            .status(400)
+            .json({
+                success: false,
+                message: 'Invalid ID',
+            })
+    }
     // look for document with provided id
     let document = await Document.findById(request.params.id).exec();
-
     // if no document was found
     if (!document){
         result
@@ -239,7 +267,7 @@ router.patch('/:id/report', async (request, result) => {
             })
         return;
     }
-
+    // if document was already reported, do nothing and return
     if(document.reported){
         return result
             .status(200)
@@ -248,18 +276,29 @@ router.patch('/:id/report', async (request, result) => {
                 message: 'Document was already reported and is being evaluated'
             });
     }
-
+    // if here, document was not already reported
     // update reported attribute
     document.reported = true;
     // push changes to database
-    await document.save();
-
-    return result
-        .status(200)
-        .json({ 
-            success: true,
-            message: 'Document reported successfully'
-        });
+    document.save()
+        .then( () => {
+            // document report was successfull
+            return result
+                .status(200)
+                .json({ 
+                    success: true,
+                    message: 'Document reported successfully'
+                });
+        })
+        .catch( error => {
+            // document report failed
+            return result
+                .status(400)
+                .json({
+                    success: false,
+                    message: error.message
+                })
+        })
 })
 
 module.exports = router;
