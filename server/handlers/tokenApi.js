51110096c436c0e2f46081421297bcb987201edd
manuelcattelan <cattelanmanuel07@gmail.com>
2022-05-28 23:15:21 +0200

    remove unused libraries

--- server/handlers/tokenApi.js
+++ server/handlers/tokenApi.js
@@ -1,8 +1,5 @@
 const express = require('express');
-require('dotenv').config();
 const router = express.Router();
-
-const jwt = require('jsonwebtoken');
 
 router.get('/', async (req, res) => {
     res.status(200).json({ success: true, tokenData: req.loggedUser});
