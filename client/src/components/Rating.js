5e4494df0bc9c5ced3ea290e20d2c1605ce7b764
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2022-05-29 16:34:19 +0200

    Update Rating.js with real time ui update

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,16 +1,47 @@
 import React, {useState} from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 
-const Rating = ({like, dislike, id, liked, token}) => {
+const Rating = ({like, dislike, id, rating, token, saved}) => {
 
 
     const handleLike = (e) => {
-        
-        fetch("http://localhost:3001/api/v1/documents/"+id+"/like?token="+token, {method: 'POST'})
+        fetch("http://localhost:3001/api/v1/documents/"+id+"/like?token="+token, {method: 'PATCH'})
         .then(res => res.json())
         .then(data => {
             if(data.success){
-                
+                if(data.rating === "liked"){
+                    document.getElementById("t-u").className="bi bi-hand-thumbs-up-fill"
+                    document.getElementById("t-d").className="bi bi-hand-thumbs-down"
+                    document.getElementById("like").innerHTML = "Piace a " + data.like +" utenti"
+                    document.getElementById("dislike").innerHTML = "Non piace a " + data.dislike +" utenti"
+                }
+                else{
+                    document.getElementById("t-u").className="bi bi-hand-thumbs-up"
+                    document.getElementById("t-d").className="bi bi-hand-thumbs-down"
+                    document.getElementById("like").innerHTML = "Piace a " + data.like +" utenti"
+                    document.getElementById("dislike").innerHTML = "Non piace a " + data.dislike +" utenti"
+                }
+            }
+        })
+    }
+
+    const handleDislike = (e) => {
+        fetch("http://localhost:3001/api/v1/documents/"+id+"/dislike?token="+token, {method: 'PATCH'})
+        .then(res => res.json())
+        .then(data => {
+            if(data.success){
+                if(data.rating === "disliked"){
+                    document.getElementById("t-u").className="bi bi-hand-thumbs-up"
+                    document.getElementById("t-d").className="bi bi-hand-thumbs-down-fill"
+                    document.getElementById("like").innerHTML = "Piace a " + data.like +" utenti"
+                    document.getElementById("dislike").innerHTML = "Non piace a " + data.dislike +" utenti"
+                }
+                else{
+                    document.getElementById("t-u").className="bi bi-hand-thumbs-up"
+                    document.getElementById("t-d").className="bi bi-hand-thumbs-down"
+                    document.getElementById("like").innerHTML = "Piace a " + data.like +" utenti"
+                    document.getElementById("dislike").innerHTML = "Non piace a " + data.dislike +" utenti"
+                }
             }
         })
     }
@@ -21,25 +52,33 @@ const Rating = ({like, dislike, id, liked, token}) => {
         <>
             <Row>
                 <Col xs="auto">
+
                     {
-                        liked
+                        rating === "liked" 
                         ?
-                        <i class="bi bi-hand-thumbs-down-fill" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleLike}></i>
+                        <>
+                            <i id="t-d" class="bi bi-hand-thumbs-down" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleDislike}></i>
+                            <i id="t-u" class="bi bi-hand-thumbs-up-fill" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleLike}></i>
+                        </>
                         :
-                        <i class="bi bi-hand-thumbs-down" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleLike}></i>
+                            rating === "disliked"
+                            ?
+                            <>
+                                <i id="t-d" class="bi bi-hand-thumbs-down-fill" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleDislike}></i>
+                                <i id="t-u" class="bi bi-hand-thumbs-up" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleLike}></i>
+                            </>
+                            :
+                            <>
+                                <i id="t-d" class="bi bi-hand-thumbs-down" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleDislike}></i>
+                                <i id="t-u" class="bi bi-hand-thumbs-up" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleLike}></i>
+                            </>
                     }
-                    {
-                        liked
-                        ?
-                        <i class="bi bi-hand-thumbs-up-fill" style={{fontSize: "2rem", color: "#623FF0"}} onClick={handleLike}></i>
-                        :
-                        <i class="bi bi-hand-thumbs-up" style={{fontSize: "2rem", color: "#623FF0"}}  onClick={handleLike}></i>
-                    }
+                    
                     
                 </Col>
                 <Col className="mt-2 p-0">
-                    <p className='my-0 text-primary doc-descrizione' style={{lineHeight:"15px"}}>Piace a {like} utenti</p>
-                    <p className='my-0 text-primary doc-descrizione' style={{lineHeight:"15px"}}>Non piace a {dislike} utenti</p>
+                    <p id="like" className='my-0 text-primary doc-descrizione' style={{lineHeight:"15px"}}>Piace a {like} utenti</p>
+                    <p id="dislike" className='my-0 text-primary doc-descrizione' style={{lineHeight:"15px"}}>Non piace a {dislike} utenti</p>
                 </Col>
             </Row>
             
