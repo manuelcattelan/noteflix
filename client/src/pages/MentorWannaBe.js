39bb0df4d4ca8a9e0e2ce4b9fad8a3de19e0a62a
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2022-06-01 16:07:37 +0200

    Update MentorWannaBe.js

--- client/src/pages/MentorWannaBe.js
+++ client/src/pages/MentorWannaBe.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import Navigation from '../components/Navigation';
+import { useAlert } from 'react-alert'
 import img from '../media/mentorwannabe.svg'
 
 
@@ -9,9 +10,12 @@ import img from '../media/mentorwannabe.svg'
 const MentorWannaBe = (props) => {
 
     const navigate = useNavigate();
+    const alert = useAlert()
+
+    const token = JSON.parse(window.localStorage.getItem("token"))
 
     useEffect(() => {
-        fetch("http://localhost:3001/api/v1/token/?token="+props.token)
+        fetch("http://localhost:3001/api/v1/token/?token="+token)
         .then(resp => resp.json())
         .then(data => {
             if(!data.success){
@@ -20,33 +24,29 @@ const MentorWannaBe = (props) => {
         })
     }, []);
 
+    const handleRequest = () => {
+        // fetch("api/v1//users/changeSubscription?token="+token, {
+        //     method: 'PATCH',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify( { subscriptionType: subplan, subscriptionArea: macroarea } ),
+
+        // })
+        // .then(res => res.json())
+        // .then(data => alert.show(data.message))
+    }
+
 
     return (
         <>
             <Navigation navbar={props.navbar} token={props.token}/>
-            <Container className='d-flex justify-content-center align-items-center' style={{height:"90vh"}}>
+            <Container className='d-flex flex-column justify-content-center align-items-center' style={{height:"90vh"}}>
                 <div>
                     <p className='titolo text-center'><span className='text-primary'>Guadagnare</span> vendendo <span className='text-primary'>appunti</span> di <br/> qualità sulla nostra piattafroma.</p>
                     <p className='testo text-center'>Prima di cominciare permetti al nostro team di valutare il tuo profilo.</p>
-                        
-                    <Row className='mt-5'>
-                        <Col xs="12" lg="4">
-                            <img src={img} alt="diventa mentor" style={{width:"90%"}}/>
-                        </Col>
-                        <Col>
-                            <p className='special mb-4 mt-3'>Mandaci un’email con le seguenti <br/>informazioni e parlaci di te.</p>
-                            <p className='testo mb-4'>
-                                <ul>
-                                    <li>Una video presentazione breve (5 minuti massimo) in cui parli di te e di cosa fai nella vita.</li>
-                                    <li>Una breve intorduzione (3 righe) del motivo che ti spinge a diventare Mentor su Noteflix.</li>
-                                    <li>Due allegati contenenti le tue dispense più recenti per valutare il tuo stile.</li>
-                                    <li>Il tuo titolo di studio più recente in allegato.</li>
-                                </ul>
-                            </p>
-                            <Button>Scrivici un'email</Button>
-                        </Col>
-                    </Row>
+                    {/* <img src={img} alt="diventa mentor" style={{width:"20rem"}}/> */}
+                    <p className='special mb-4 mt-3 text-center'>Effettua la rischiesta e <span>diventa mentore</span>.</p>
                 </div>
+                <Button className="text-center" onClick={handleRequest}>Richiedi</Button>
             </Container>
             
         </>
