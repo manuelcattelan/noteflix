bc5c712e4e6407a6a27a8fdd0e17edb882c6c490
manuelcattelan <cattelanmanuel07@gmail.com>
2022-06-01 09:45:37 +0200

    add database connection test

--- server/test/test.js
+++ server/test/test.js
@@ -3,11 +3,24 @@ const app = require('../app.js');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
-test('app module should be defined', () => {
-    expect(app).toBeDefined();
-});
-test('GET / should return 200', () => {
-    return request(app)
-    .get('/api/v1/documents')
-    .expect(200);
-});
+describe('GET /api/v1/documents', () => {
+    beforeAll( async () => {
+        jest.setTimeout(8000);
+        jest.unmock('mongoose');
+        db_connection = await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Database successfully connected!");
+    })
+
+    afterAll( async () => {
+        mongoose.disconnect();
+        console.log("Database connection successfully closed.");
+    })
+    test('app module should be defined', () => {
+        expect(app).toBeDefined();
+    });
+    test('GET / should return 200', () => {
+        return request(app)
+            .get('/api/v1/documents')
+            .expect(200);
+    });
+})
