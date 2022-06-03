fd5409a24d4025f02ced18156bfd697a7f208ad5
guss-alberto <96133902+guss-alberto@users.noreply.github.com>
2022-06-03 16:01:34 +0200

    add test cases

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -1,46 +1,39 @@
 const request = require('supertest');
 const app = require('../app.js');
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 
 
-describe('GET /api/v1/documents', () => {
+describe('', () => {
     let userSpy;
+    let userSaveSpy;
     beforeAll( async () => {
-        jest.setTimeout(8000);
-        /*jest.unmock('mongoose');
-        db_connection = await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("Database successfully connected!");*/
+        jest.setTimeout(100000);
 
         const User = require('../models/userModel');
+
+        //mock find user function to return a mock user
         userSpy = jest.spyOn(User, 'findOne').mockImplementation((criterias) => {
             let ret;
             if (criterias.email=="utente@gmail.com")
-                ret = {"_id":{"$oid":"629212873e064e49f55addc3"},"email":"utente@gmail.com","passwordHash":"9c142d288c143c208a43afd5aa48a4d64f4f3da1c73770f8f30cdefdedf631af","passwordSalt":"kxKto29XMW9KUvyoCxA1mA==","joinDate":{"$date":{"$numberLong":"1653740167601"}},"userType":"user","username":"utentenerd","subscription":{"subType":"nerd","area":"","creationDate":{"$date":{"$numberLong":"1653740167601"}},"lastPayment":{"$date":{"$numberLong":"1653740167601"}}},"savedDocuments":[],"avatar":{"sex":"woman","faceColor":"#F9C9B6","earSize":"big","eyeStyle":"circle","noseStyle":"long","mouthStyle":"laugh","shirtStyle":"polo","glassesStyle":"none","hairColor":"#F48150","hairStyle":"womanLong","hatStyle":"none","hatColor":"#F48150","eyeBrowStyle":"up","shirtColor":"#6BD9E9","bgColor":"#74D153"},"__v":{"$numberInt":"8"}};
-                   
-            return {
-                exec: function (){
-                        return  ret;
-                }
-            }
-        
+                ret = { "email":"utente@gmail.com",
+                        "passwordHash":"9c142d288c143c208a43afd5aa48a4d64f4f3da1c73770f8f30cdefdedf631af", //password is 'password'
+                        "passwordSalt":"kxKto29XMW9KUvyoCxA1mA=="
+                    }
+            return {exec: ()=> { return  ret; }};
+        });
+
+        //mock save function to prevent writes to DB
+        userSaveSpy = jest.spyOn(User.prototype, 'save').mockImplementation(async (criterias) => {
+            return true;
         });
     })
     
     afterAll( async () => {
-        mongoose.disconnect();
+        //mongoose.disconnect();
         console.log("Database connection successfully closed.");
         userSpy.mockRestore();
+        userSaveSpy.mockRestore();
     })
-
-    /*let token = jwt.sign({
-        id: '629212873e064e49f55addc3',
-        type: 'user',
-        subscription: {
-            type: 'nerd',
-            area: ''
-        }
-    },process.env.TOKEN_SECRET, { expiresIn: 86400 });*/
     
     test('app module should be defined', () => {
         expect(app).toBeDefined();
@@ -57,6 +50,20 @@ describe('GET /api/v1/documents', () => {
             .post('/api/v2/auth/login')
             .set('Accept', 'application/json')
             .send({ email: 'utente@gmail.com' }) 
+            .expect(400);
+    });
+    test('LOGIN / Without email return 400', () => {
+        return request(app)
+            .post('/api/v2/auth/login')
+            .set('Accept', 'application/json')
+            .send({ password: 'password' }) 
+            .expect(400);
+    });
+    test('LOGIN / With non-registered email return 400', () => {
+        return request(app)
+            .post('/api/v2/auth/login')
+            .set('Accept', 'application/json')
+            .send({ email: 'utente_falso@gmail.com', password: 'password' }) 
             .expect(400);
     });
     test('LOGIN / Without username return 400', () => {
@@ -80,4 +87,143 @@ describe('GET /api/v1/documents', () => {
             .send({ email: 'utente@gmail.com', password: 'password_sbagliata'}) 
             .expect(400);
     });
+
+    // SIGNUP
+    test('SIGNUP / with already used email should return 400', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'utente@gmail.com', 
+                    password: 'password',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                }) 
+            .expect(400);
+    });
+    test('SIGNUP / with invalid email format should return 400', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'utent7/egmail.com', 
+                    password: 'password',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                }) 
+            .expect(400);
+    });
+
+    test('SIGNUP / With invalid subscription plan', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "falso",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(400);
+    });
+
+    test('SIGNUP / with new email should return 201', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(201);
+    });
+
+    test('SIGNUP / with missing email', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ password: 'password',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(400);
+    });
+
+    test('SIGNUP / with missing password', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: '',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(400);
+    });
+
+    test('SIGNUP / with missing subscription', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(400);
+    });
+    test('SIGNUP / with missing scubscription area in "studenti" plan', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "studenti",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(400);
+    });
+    test('SIGNUP / with missing avatar', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "nerd",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(400);
+    });
+    test('SIGNUP / with missing username', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: ''
+                 }) 
+            .expect(400);
+    });
+
+    test('SIGNUP / with new email should return 201', () => {
+        return request(app)
+            .post('/api/v2/auth/signup')
+            .set('Accept', 'application/json')
+            .send({ email: 'uetnte2@yahoo.com', 
+                    password: 'password',
+                    subscriptionType: "nerd",
+                    avatar: " ",
+                    username: 'Utente TEST'
+                 }) 
+            .expect(201);
+    });
+
 })  
---
+++ server/test/document.test.js
@@ -0,0 +1,8 @@
+/*let token = jwt.sign({
+        id: '629212873e064e49f55addc3',
+        type: 'user',
+        subscription: {
+            type: 'nerd',
+            area: ''
+        }
+    },process.env.TOKEN_SECRET, { expiresIn: 86400 });*/
