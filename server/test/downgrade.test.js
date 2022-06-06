a6ae27193bd6190d5c5b359c02e1f55d6e64bd5a
manuelcattelan <cattelanmanuel07@gmail.com>
2022-06-06 10:24:38 +0200

    add env variables

--- server/test/downgrade.test.js
+++ server/test/downgrade.test.js
@@ -2,7 +2,7 @@ const request  = require('supertest');
 const app      = require('../app');
 const jwt      = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 
 describe('Downgrade user test', () => {
     let token
